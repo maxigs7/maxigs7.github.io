98bd53ec3dfa459f45bc147cef30e26f91360b2f
Maximiliano Gonzalez Schweizer <maxi.g14@hotmail.com>
2019-04-11 19:12:26 -0300

    experience card

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -7,4 +7,20 @@ $(function() {
         var target = $(this).data('target');
         $(target).toggleClass('collapse');
     });
+
+    $('[data-ago-from]').each(function(i, el) {
+        var $this = $(this);
+        var from = $this.data('ago-from');
+        var to = $this.data('ago-to');
+        var time;
+        if (to) {
+            time = moment(from, 'MM/DD/YYYY').from(
+                moment(to, 'MM/DD/YYYY'),
+                true
+            );
+        } else {
+            time = moment(from, 'MM/DD/YYYY').fromNow(true);
+        }
+        $this.text('(' + time + ')');
+    });
 });
